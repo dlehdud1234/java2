0c123d0126adfcf85d56331e52b81f5a0f1a5327
dlehdud1234 <as007564@gmail.com>
2025-05-22 11:15:20 +0900

    11

---
+++ test.java
@@ -0,0 +1,14 @@
+class Point3[
+    private int x, y;
+    public Point3(int x, int y)[
+        this.x = x; this.y = y;
+    ]
+]
+
+public class Ex61ObjectPropertyEx[
+    public static void main(String [] args)[
+        Point p = new Point(x:2, y:3);
+        System.out.println(p.getClass().getName());
+        
+    ]
+]
